89bb14c45e6222dd97d8648d5dace6fd59adf179
Bzq <56465@qq.com>
2018-11-29 22:38:49 +0800

    权限列表框架完成

--- src/components/role/role.vue
+++ src/components/role/role.vue
@@ -7,7 +7,25 @@
       :data="rightlist"
       style="width: 100%"
       >
-      <el-table-column type="expand"> </el-table-column>      
+     <el-table-column type="expand"  width="80">
+          <template slot-scope="scope">
+             <el-row v-for="(item1,index) in scope.row.children" :key="index">
+               <el-col>
+                 <el-tag>{{item1.authName}}</el-tag>
+               </el-col>
+               <el-col >
+                 <el-row>
+                   <el-col v-for="(item2,i) in scope.row.children[index].children" :key="i">
+                     <el-tag>{{item2.authName}}</el-tag>
+                   </el-col>
+                   <el-col>
+                     <el-tag></el-tag>
+                   </el-col>
+                 </el-row>
+               </el-col>
+             </el-row>
+          </template> 
+     </el-table-column>      
       <el-table-column type="index" label="#" width="180" > </el-table-column>
 
       <el-table-column
@@ -51,8 +69,12 @@ export default {
         methods:{
           async  getlist(){
                 const res = await this.$http.get(`roles`)
-                console.log(res)
+                // console.log(res.data.data)
                 this.rightlist=res.data.data
+               console.log(res.data.data[0].children )  
+            //  console.log(res.data.data.children.children )  
+                                                                          
+                
             },
         },
       
@@ -65,4 +87,7 @@ export default {
     margin-top: 20px;
     margin-bottom: 20px;
 }
+
+
+
 </style>
